b57b2a15b20165fcccb744ea4f80491e218b95bd
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-10 14:37:00 -0500

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Suguru Tokuda on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
