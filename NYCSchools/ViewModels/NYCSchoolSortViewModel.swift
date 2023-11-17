487d646434ec8c469818763a6b98e741c00c04aa
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-17 09:21:45 -0500

    Added a new view model.

--- NYCSchools/ViewModels/NYCSchoolSortViewModel.swift
+++ NYCSchools/ViewModels/NYCSchoolSortViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class NYCSchoolSortViewModel {
+    private var sortOrder: SortOrder = .asc
+    private var sortKey: NYCSchoolSortKey = .schoolName
+    
+    func setValues(sortKey: NYCSchoolSortKey, sortOrder: SortOrder) {
+        self.sortOrder = sortOrder
+        self.sortKey = sortKey
+    }
+    
+    func setSortOrder(sortOrder: SortOrder) {
+        self.sortOrder = sortOrder
+    }
+    
+    func setSortKey(sortKey: NYCSchoolSortKey) {
+        self.sortKey = sortKey
+    }
+    
+    func getSelectionValues() -> (NYCSchoolSortKey, SortOrder) {
+        return (sortKey, sortOrder)
+    }
+}
