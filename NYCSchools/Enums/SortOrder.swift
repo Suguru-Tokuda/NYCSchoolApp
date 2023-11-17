87f2e209c572f088daa588b58d05659273201924
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-17 09:20:42 -0500

    Added a new enum.

--- NYCSchools/Enums/SortOrder.swift
+++ NYCSchools/Enums/SortOrder.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum SortOrder: String, CaseIterable {
+    case asc = "Ascending", dsc = "Descending"
+    
+    func getSortOrderStr() -> String {
+        switch self {
+        case .asc:
+            return "ASC"
+        case .dsc:
+            return "DESC"
+        }
+    }
+}
